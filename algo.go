75735316ea19ca1b9647057e3ef159f6aa2981b6
your_github_username <paerarason1111@gmail.com>
2023-10-28 17:41:33 +0530

    added pre

--- algo.go
+++ algo.go
@@ -1,6 +1,4 @@
-
 package main
-
 import "fmt"
 
 type Activity struct {
@@ -8,36 +6,13 @@ Name       string
 Duration   int
 Predecessors []*Activity
 Successors   []*Activity
-// You can include other fields as needed.
 }
 
 
-func main() {
-// Create activities
-A := &Activity{Name: "A", Duration: 3}
-B := &Activity{Name: "B", Duration: 4}
-C := &Activity{Name: "C", Duration: 2}
-D := &Activity{Name: "D", Duration: 5}
-E := &Activity{Name: "E", Duration: 2}
-
-// Define dependencies
-B.Predecessors = append(B.Predecessors, A)
-C.Predecessors = append(C.Predecessors, A)
-D.Predecessors = append(D.Predecessors, B, C)
-E.Predecessors = append(E.Predecessors, D)
-
-// Update successors
-A.Successors = append(A.Successors, B, C)
-B.Successors = append(B.Successors, D)
-C.Successors = append(C.Successors, D)
-D.Successors = append(D.Successors, E)
-
-// Now you have a CPM graph where each activity can have multiple predecessors and successors.
-
-fmt.Printf("Activity A Predecessors: %v, Successors: %v\n", A.Predecessors, A.Successors)
-fmt.Printf("Activity D Predecessors: %v, Successors: %v\n", D.Predecessors, D.Successors)
-}
-
+//place predecessors to the particular task 
+func (task *Activity) AddPredecessor(pre *Activity){
+     append(task.Predecessors,pre)
+   }
 
 /*{
 		
@@ -65,13 +40,13 @@ fmt.Printf("Activity D Predecessors: %v, Successors: %v\n", D.Predecessors, D.Su
       {,
          "name":"E",
          "previous":["C"],
-         "time":1
-      },
+      
       {
          "name":"F",
          "previous":["D"],
          "time":1
       }]
-   }
+  
+}
    */
    
